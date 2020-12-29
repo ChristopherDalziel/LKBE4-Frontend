ce630c60bf7ce70277ac87bf99b4f6a5218dcc55
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-12-29 13:40:11 +1100

    installed axios, switched to functional component w/hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,68 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Api from './Api';
-import GameList from "./GameList"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Api from './Api';
+// import GameList from "./GameList"
 
 
-class App extends Component {
+// class App extends Component {
 
-  componentDidMount() {
-    const apiUrl = 'http://localhost:6060/games';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
-  }
+//   componentDidMount() {
+//     const apiUrl = 'http://localhost:6060/games';
+//     fetch(apiUrl)
+//       .then((response) => response.json())
+//       .then((data) => console.log('This is your data', data));
+//   }
 
-  render() {
+//   render() {
 
-    return (
-      // <Router>
-      //   <Switch>
-      //     <Route
-      //       path='/'
-      //       exact={true}
-      //       render={(props) => <GameList {...props} api={api} />}
-      //     />
-      //   </Switch>
-      // </Router>
-      <div>hello</div>
-    )
+//     return (
+//       // <Router>
+//       //   <Switch>
+//       //     <Route
+//       //       path='/'
+//       //       exact={true}
+//       //       render={(props) => <GameList {...props} api={api} />}
+//       //     />
+//       //   </Switch>
+//       // </Router>
+//       <div>hello</div>
+//     )
+//   }
+// }
+
+// export default App;
+
+function App() {
+  const [state, setState] = useState({
+    games: [],
+    status: 'loading',
+    error: null
+  });
+  const [url] = useState('http://localhost:6060/games')
+  const { status, games } = state
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setState({ status: 'resolved', games: res.data })
+      })
+  }, [url])
+
+
+  if (status === 'loading') {
+    return <div>loading</div>
+  } else if (status === 'rejected') {
+    throw console.log(status)
+  } else if (status === 'resolved') {
+    return <ul>
+      {games.map(item => (
+        <li>
+          <p>{item.name}</p>
+        </li>
+      ))}
+    </ul>
   }
 }
 
