62d06f834f319cb4654d434caf2522358afe6b9f
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-12-30 09:08:45 +1100

    removed extra files, removed commented imports

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Api from './Api';
-// import GameList from "./GameList"
-
-
-// class App extends Component {
-
-//   componentDidMount() {
-//     const apiUrl = 'http://localhost:6060/games';
-//     fetch(apiUrl)
-//       .then((response) => response.json())
-//       .then((data) => console.log('This is your data', data));
-//   }
-
-//   render() {
-
-//     return (
-//       // <Router>
-//       //   <Switch>
-//       //     <Route
-//       //       path='/'
-//       //       exact={true}
-//       //       render={(props) => <GameList {...props} api={api} />}
-//       //     />
-//       //   </Switch>
-//       // </Router>
-//       <div>hello</div>
-//     )
-//   }
-// }
-
-// export default App;
 
 const App = () => {
   const [state, setState] = useState({
