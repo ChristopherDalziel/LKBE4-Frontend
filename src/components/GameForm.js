99cffac34c77a02b762b20904362a94dcc52cbb5
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2021-02-14 17:49:27 +1100

    cleaned up game form to make it a dumb component, moved logic to game container

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,87 +1,27 @@
-import axios from 'axios'
 import React, { useState } from 'react'
 import "../css/GameForm.css"
 
-// ** GO OVER SOME EPIC/REACT ABOUT CONTROLLED / UNCONTROLED INPUTS 
-// REMOVE STATE TO CREATE 'DUMB' COMPONENTS - PASS LOGIC IN
-
-const GameForm = (props = "") => {
-    const [gameData, setGameData] = useState(props.gameData ? props.gameData : { name: "", releaseYear: 0, rating: '', developer: "" })
-
-    const handleInputChange = e => {
-        const { name, value } = e.target
-        setGameData(props.gameData ? { ...props.gameData, [name]: value } : { ...gameData, [name]: value })
-    }
-
-    const onSubmit = e => {
-        // Controlled v uncontrolled inputs
-        // const { name, value } = e.target
-        e.preventDefault()
-        axios
-            .post(process.env.REACT_APP_URL, gameData)
-            .catch(function (error) {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log("response.data: ", error.response.data);
-                    console.log("status: ", error.response.status);
-                    console.log("headers: ", error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                    // http.ClientRequest in node.js
-                    console.log("request error: ", error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error: ", error.message);
-                }
-                console.log("error config: ", error.config);
-            });
-        window.location.reload()
-    }
-
-    const onUpdate = e => {
-        e.preventDefault()
-        axios
-            .put(process.env.REACT_APP_URL, gameData)
-            .catch(function (error) {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log("response.data: ", error.response.data);
-                    console.log("status: ", error.response.status);
-                    console.log("headers: ", error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                    // http.ClientRequest in node.js
-                    console.log("request error: ", error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error: ", error.message);
-                }
-            });
-        window.location.replace('/')
-    }
-
+const GameForm = ({ onGameChange, onGameSubmit }) => {
     return (
-        <form onSubmit={props.gameData ? onUpdate : onSubmit}>
-            {props.gameData ?
-                <><label id="_id" htmlFor="_id">ID: </label> <br />
-                    <input type="text" name="_id" value={props.gameData._id} /> </>
-                : ''}
+        <form onSubmit={onGameSubmit}>
+            {/* {props.gameData ?
+                <>
+                    <label id="_id" htmlFor="_id">ID: </label> <br />
+                    <input type="text" name="_id" value={props.gameData._id} />
+                </>
+                : ''} */}
             <br />
             <label id="name" htmlFor="name">Name: </label>
-            <input type="text" name="name" placeholder="Game title" onChange={handleInputChange} defaultValue={props.gameData ? props.gameData.name : ''} />
+            <input type="text" name="name" placeholder="Game title" onChange={onGameChange} />
             <br />
             <label htmlFor="releaseYear">Year: </label>
-            <input type="number" name="releaseYear" placeholder="Release year" onChange={handleInputChange} defaultValue={props.gameData ? props.gameData.releaseYear : ''} />
+            <input type="number" name="releaseYear" placeholder="Release year" onChange={onGameChange} />
             <br />
             <label htmlFor="rating">Rating: </label>
-            <input type="text" name="rating" placeholder="Age rating" onChange={handleInputChange} defaultValue={props.gameData ? props.gameData.rating : ''} />
+            <input type="text" name="rating" placeholder="Age rating" onChange={onGameChange} />
             <br />
             <label htmlFor="developer">Developer: </label>
-            <input type="text" name="developer" placeholder="Developer" onChange={handleInputChange} defaultValue={props.gameData ? props.gameData.developer : ''} />
+            <input type="text" name="developer" placeholder="Developer" onChange={onGameChange} />
             <br />
             <input type="submit" value="Click Me"></input>
         </form>
